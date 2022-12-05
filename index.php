1a365228985c36e819bc8c07d04c473ab93114b8
SainasFede <sainasfede@gmail.com>
2022-12-05 15:36:05 +0100

    add colors

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="./style/styles.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js" integrity="sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
@@ -16,28 +17,28 @@
 <body>
 
 <div id="app">
-  <header class="text-center">
-    <h1>Dischi Spotify</h1>
+  <header>
+    <div class="container h-100 d-flex justify-content-center align-items-center justify-content-between">
+      <i class="fa-brands fa-spotify fa-2xl"></i>
+      <h2>Dischi Spotify</h2>
+    </div>
   </header>
 
-  <div class="container">
-    <div class="row d-flex flex-wrap justify-content-center">
-      <div v-for="(disco, index) in dischi" :key="index" class="card m-4" style="width: 18rem;">
-        <img :src="disco.poster" class="card-img-top" alt="">
-      <div class="card-body">
-        <h5 class="card-title">{{disco.title}}</h5>
-        <p class="card-text">{{disco.author}}</p>
-        <p class="card-text">{{disco.year}}</p>
+  <main>
+    <div class="container card-wrap">
+      <div class="row d-flex flex-wrap justify-content-center">
+        <div v-for="(disco, index) in dischi" :key="index" class="card m-4 p-4 text-center" style="width: 18rem;">
+          <img :src="disco.poster" class="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title">{{disco.title}}</h5>
+          <p class="card-text">{{disco.author}}</p>
+          <p class="card-text">{{disco.year}}</p>
+        </div>
       </div>
     </div>
-  </div>
+  </main>
 
 </div>
-
-
-
-  </div>
-
 
   <script src="./js/main.js"></script>
 </body>
